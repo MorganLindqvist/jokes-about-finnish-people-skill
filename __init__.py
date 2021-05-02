b43f703b3ba9ff58f15b415ce6812e6e426e9fdf
Morgan Lindqvist <Morgan.Lindqvist@quzed.com>
2021-05-02 08:18:21 +0200

    Update to new structure
    Change-Id: I94832291ae755f313abfd7e254ce57758874ed9a

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
 
 class JokesAboutFinnishPeople(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+       super().__init__()
+        self.learning = True
 
-    @intent_file_handler('people.finnish.about.jokes.intent')
-    def handle_people_finnish_about_jokes(self, message):
+    @intent_handler('people.finnish.about.jokes.intent')
+    def handle_people_finnish_about_jokes_intent(self, message):
         self.speak_dialog('people.finnish.about.jokes')
+
+    def stop(self):
+        pass
 
 
 def create_skill():
     return JokesAboutFinnishPeople()
-
