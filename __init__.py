f56539d752bbcf99330fadd0c8d0e59d73a5d3fd
Morgan Lindqvist <Morgan.Lindqvist@quzed.com>
2021-06-12 19:43:17 +0200

    Fixed indentation

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from mycroft import MycroftSkill, intent_handler
 
 class JokesAboutFinnishPeople(MycroftSkill):
     def __init__(self):
-       super().__init__()
+        super().__init__()
         self.learning = True
 
     @intent_handler('people.finnish.about.jokes.intent')
